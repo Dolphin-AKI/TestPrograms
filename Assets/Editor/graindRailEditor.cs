e468ef5bfb32ea8040661d950765f55ea5a99c71
Dolphin-AKI <aquastar.dolphin@gmail.com>
2016-06-27 18:29:09 +0900

    GraindRailSystem Add

---
+++ Assets/Editor/graindRailEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[@CustomEditor (typeof(graindRail))]
+public class graindRailEditor : Editor {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSceneGUI()
+    {
+        graindRail gr = target as graindRail;
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 pos = Handles.PositionHandle(gr.pathT, Quaternion.identity);
+
+        if (EditorGUI.EndChangeCheck()){
+            Undo.RecordObject(gr, "Move graindrail Path");
+            gr.pathT = pos;
+            gr.EditorUpdate();
+        }
+    }
+}
---
+++ Assets/GraindRailSystem/graindRail.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class graindRail : MonoBehaviour {
+
+    public int pathLength;
+    private List<Transform> path;
+
+    
+    public Vector3 pathT;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void EditorUpdate()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+
+    }
+}
