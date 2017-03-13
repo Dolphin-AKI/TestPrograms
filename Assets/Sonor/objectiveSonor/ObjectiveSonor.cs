a859c43d9e37d3006a123e895b2fdcb395bc95b2
Dolphin-AKI <aquastar.dolphin@gmail.com>
2017-03-13 19:09:49 +0900

    [AQ] ObjectiveSonar 書き始め
    マテリアルとかファイルを作成してちょっと書いてみたが動かないorz

---
+++ Assets/Sonor/objectiveSonor/ObjectiveSonor.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveSonor : MonoBehaviour {
+
+    public enum ObjectType
+    {
+        none,
+        enemy,
+        item,
+        friend,
+    }
+    public ObjectType Type = ObjectType.none;
+    public Color color;
+    public float smoothness;
+    public float metalic;
+
+    private float _glowtime = 1.5f;
+    public float GlowTime { get { return _glowtime; } set { _glowtime = value; } }
+
+    private float timer = 0;
+
+    private float _amplitude = 0;
+
+    [SerializeField]
+    Shader shader;
+
+    //shader properties
+    int ColorID;
+    int SmoothnessID;
+    int MetalicID;
+    int GlowID;
+
+
+    private void Awake()
+    {
+        ColorID = Shader.PropertyToID("_Color");
+        SmoothnessID = Shader.PropertyToID("_Glossiness");
+        MetalicID = Shader.PropertyToID("_Metallic");
+        GlowID = Shader.PropertyToID("_Glow");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Shader.SetGlobalColor(ColorID, color);
+        Shader.SetGlobalFloat(SmoothnessID, smoothness);
+        Shader.SetGlobalFloat(MetalicID, metalic);
+
+
+        if (Input.GetKeyDown("s"))
+        {
+            timer = 0;
+        }
+
+
+        if(timer < _glowtime)
+        {
+
+            _amplitude = Mathf.Cos( timer / _glowtime * Mathf.PI / 2);
+            
+            Shader.SetGlobalFloat(GlowID, _amplitude);
+
+            timer += Time.deltaTime;
+        }
+        else
+        {
+
+        }
+
+
+
+
+		
+	}
+}
---
+++ Assets/Sonor/objectiveSonor/Shader/ObjectSonor.shader
@@ -0,0 +1,40 @@
+﻿Shader "AQ_Sonor/ObjectSonor" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+		_Glow("Glow", Float) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Transparent" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Lambert alpha
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		
+
+		struct Input {
+			float4 color : COLOR;
+		};
+
+	
+		float _Glow;
+		
+		float3 _Color;
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			// Albedo comes from a texture tinted by color
+			o.Albedo = _Color.rgb;
+			// Metallic and smoothness come from slider variables
+			
+			o.Alpha = _Glow;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
